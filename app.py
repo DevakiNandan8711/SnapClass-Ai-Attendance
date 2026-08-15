feeb88885f3c8ccefbea35100732fc3f9a1820bd
DevakiNandan8711 <devakinandan076@gmail.com>
2026-08-15 12:05:13 +0530

    Finishing Touches

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ from src.screens.home_screen import home_screen
 from src.screens.student_screen import student_screen
 from src.screens.teacher_screen import teacher_screen
 
+from src.components.dialog_auto_enroll import auto_enroll_dialog
 
 def main():
+    st.set_page_config(
+        page_title="SnapClass - Making Attendance faster using AI",
+        page_icon="https://i.ibb.co/YTYGn5qV/logo.png"
+    )
     
     
     if 'login_type' not in st.session_state:
@@ -23,6 +28,16 @@ def main():
         case None:
             home_screen()
     
+    join_code = st.query_params.get("join")
+    
+    if join_code:
+        if st.session_state.login_type!='student':
+            st.session_state.login_type = 'student'
+            st.rerun()
+
+        if st.session_state.get('is_logged_in') and st.session_state.get('user_role') == 'student':
+            auto_enroll_dialog(join_code)
+            
     
     
        
--- src/components/header.py
+++ src/components/header.py
@@ -14,4 +14,34 @@ def header_home():
                 
                 
                 """,unsafe_allow_html=True)
+
+
+def header_dashboard():
+    
+    logo_url="https://i.ibb.co/YTYGn5qV/logo.png"
+    st.markdown(f"""
+        <div style="display:flex;  align-items:center; justify-content:center;gap:10px; margin-bottom:30px;">
+        <img src='{logo_url}'style='height:85px;'/>
+        <h2 style='text-align:left;color:#5865f2'>SNAP<br/>CLASS</h2>
+        </div>      
+                
+                
+                
+                
+                """,unsafe_allow_html=True)
+    
+    
+    
+    
+###
+    
+    
+#display: flex → Makes it easier to arrange items inside.
+#flex-direction: column → Puts items in a vertical list.
+#align-items: center → Moves items to the middle left-to-right.
+#justify-content: center → Moves items to the middle top-to-bottom.
+#margin-top & margin-bottom → Adds space above and below the whole block.
+
+
+###
     
